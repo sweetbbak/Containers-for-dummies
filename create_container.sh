fb8bf44b069a18c6c650ca459e571a410c2f9235
meili <sweetbabyalaska@gmail.com>
2023-10-06 17:47:36 -0800

    Add test shell script for automation

--- create_container.sh
+++ create_container.sh
@@ -2,14 +2,32 @@
 # auto build container creation from docker hub
 # image metadata
 
+# usage
 [ -z "${1}" ] && {
     echo -e ./"${0##*/} <container-directory> <output-dir>"
+    exit 0
 }
 
+# variables
 dir="${1}"
 output="${2}"
+
+# does output exist? if not, make it or exit
+[ ! -d "${output}" ] && mkdir -p "${output}" || exit
+
+# is manifest.json where its expected to be? If not, try to intervene or exit
+[ ! -f "${dir}/manifest.json" ] && {
+    manifest=$(find "${dir}" -iname 'manifest.json')
+    [ ! -f "${manifest}" ] && {
+        echo "Couldn't find manifest.json"
+        exit 0
+    }
+}
+
+# get a list of the container layers
 readarray layers < <(jq '.[].Layers.[]' < "${dir}/manifest.json")
 
+# iterate over the layers, layering them in our rootfs
 x=0
 for layer in "${layers[@]}"; do
     echo -e "extracting ${layer}"
